4d74fab1df8f7f93551d4f5eac0ea33d7461c0dc
MANBEERGIL <manbeergill545@gmail.com>
2020-03-06 18:47:52 -0500

    read plist

--- ReadingPlist/ViewController.swift
+++ ReadingPlist/ViewController.swift
@@ -12,8 +12,36 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        readPlistData()
         // Do any additional setup after loading the view.
     }
+    func readPlistData(){
+           if let bundlePath = Bundle.main.path(forResource: "DemoPlist", ofType: "plist"){
+               //print(bundlePath)
+               if let  dictionary = NSMutableDictionary(contentsOfFile: bundlePath){
+                   //print(dictionary)
+                   if let countries = dictionary["countries"] as? [String]{
+                       for v in countries{
+                           print(v)
+                       }
+                   }
+                if let u = dictionary["users"]as?[[String:String]]{
+                var flag = false
+                    for user in u {
+                        if user["username"] == "abc@123" && user["password"] == "man"{
+                            flag = true
+                        }
+                    }
+                    if flag == true{
+                        print("valid user")
+                    }else{
+                        print("invalid user")
+                    }
+                }
+               }
+           }
+       }
+       
 
 
 }
